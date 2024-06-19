162601e8e8cf224059aac467e67bcf831a015300
bmsk <vv99911@gmail.com>
2024-06-19 11:54:49 +0900

    메인 로고 추가

---
+++ core/designsystem/src/main/kotlin/com/time2move/beuptou/core/designsystem/theme/component/MainLogo.kt
@@ -0,0 +1,17 @@
+package com.time2move.beuptou.core.designsystem.theme.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.time2move.beuptou.core.designsystem.R
+
+@Composable
+fun MainLogo(modifier: Modifier) {
+    Image(
+        modifier = modifier,
+        imageVector = ImageVector.vectorResource(id = R.drawable.ic_main_logo),
+        contentDescription = "main logo"
+    )
+} 
